5f17693c18770777d2186352252427981426e89e
Kaviyarasi Raju <kaviyarasiraju79@gmail.com>
2026-05-17 23:04:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,26 +3,25 @@ const prices = document.querySelectorAll(".prices");
 
 let total = 0;
 
-// Calculate total price
-prices.forEach(price => {
+// Calculate total
+prices.forEach((price) => {
     total += Number(price.textContent);
 });
 
-// Select table
+// Select the table
 const table = document.querySelector("table");
 
 // Create new row
-const newRow = document.createElement("tr");
+const row = document.createElement("tr");
 
 // Create new cell
-const newCell = document.createElement("td");
+const cell = document.createElement("td");
 
-// Set colspan and total text
-newCell.colSpan = 2;
-newCell.textContent = total;
+cell.colSpan = 2;
+cell.textContent = total;
 
 // Append cell to row
-newRow.appendChild(newCell);
+row.appendChild(cell);
 
 // Append row to table
-table.appendChild(newRow);
+table.appendChild(row);
